7decd2d30006e5a272496692928fe749c61fc6c8
Ayasterisk <ayasterisk@gmail.com>
2024-12-09 15:23:46 +0700

    html

--- app.py
+++ app.py
@@ -54,7 +54,6 @@ def predict():
 
         if not selected_model:
             return render_template("index.html", error="Lựa chọn không hợp lệ.", news_text=news_text)
-        
         # Word2Vec Embedding
         if embedding_choice == "embed1":
             def document_vector(doc, word2vec_model):
@@ -65,11 +64,13 @@ def predict():
             processed_text = preprocess_text(news_text)
             if model_choice == "model1":
                 vector = document_vector(processed_text, word2vec_model).reshape(1, -1)
+                accuracy = 79.48
             else:
                 text_input = [news_text for _ in range(32)]
                 vector_list = [document_vector(preprocess_text(sentence), word2vec_model) for sentence in text_input]
                 vector_array = np.array(vector_list)
                 vector = np.expand_dims(vector_array, axis=1)
+                accuracy = 90.61
             prediction = selected_model.predict(vector)
         
         # GloVe Embedding    
@@ -80,12 +81,14 @@ def predict():
             if model_choice == "model1":                             
                 sentence_vector = np.mean(loaded_embedding_matrix[[loaded_word_index[word] for word in processed_text if word in loaded_word_index]], axis=0)
                 vector = sentence_vector.reshape(1, -1)
-                prediction = selected_model.predict(vector)                               
+                prediction = selected_model.predict(vector)  
+                accuracy = 72.38                             
             else:
                 sequence = [[loaded_word_index[word] for word in processed_text if word in loaded_word_index]]
                 padded_sequence = pad_sequences(sequence, maxlen=100, padding='post', truncating='post')
                 prediction = selected_model.predict(padded_sequence)
                 prediction = 1 - prediction[0]
+                accuracy = 86.42
                 
         # fastText Embedding
         if embedding_choice == "embed3":
@@ -102,6 +105,7 @@ def predict():
                 doc_vec = document_vector(processed_text, fasttext_model).reshape(1, -1)
                 scaled_vec = scaler.transform(doc_vec)
                 prediction = selected_model.predict(scaled_vec)
+                accuracy = 80.58
             else:
                 fasttext_model = FastText.load("models/fasttext_model.bin")
                 word_index = {word: idx + 1 for idx, word in enumerate(fasttext_model.wv.key_to_index)}
@@ -110,11 +114,13 @@ def predict():
                 sequence = text_to_sequence(processed_text, word_index)
                 padded_sequence = pad_sequences([sequence], maxlen=200, padding='post')
                 prediction = selected_model.predict(padded_sequence)[0]
+                accuracy = 84.61
+                
                              
         result = "Real" if prediction[0] < 0.5 else "Fake"
 
         return render_template(
-            "index.html", prediction=result, news_text=news_text, embedding_choice=embedding_choice, model_choice=model_choice
+            "index.html", accuracy=accuracy, prediction=result, news_text=news_text, embedding_choice=embedding_choice, model_choice=model_choice
         )
 
 if __name__ == "__main__":
